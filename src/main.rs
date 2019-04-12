9453c160d07b676fae36cd7051e641b8ca541e4e
Jakob Haahr Taankvist <jakob@jakob-T480.localdomain>
2019-04-12 15:29:48 +0200

    refactor and some benchmarks

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,11 @@
+#![feature(test)]
 mod board;
-use board::{Board, Entry};
-use std::time::{SystemTime, UNIX_EPOCH};
+mod solver;
+use board::{Board};
+use solver::fill_board;
+use std::time::{SystemTime};
 
 use std::env;
-
-fn next_cord(row: &mut usize, col: &mut usize, size: usize) {
-    if *col == size-1 {
-        *row += 1;
-        *col = 0;
-    } else {
-        *col += 1;
-    }
-}
-
-fn prev_cord(row: &mut usize, col: &mut usize, size: usize) {
-    if *col == 0 {
-        *row -= 1;
-        *col = size-1;
-    } else {
-        *col -= 1;
-    }
-}
-
-fn fill_board(board: &mut Board) {
-    let mut row = 0;
-    let mut col = 0;
-
-    'main: while row < board.size() && col < board.size() {
-        let start = match board[row][col] {
-            Entry::Empty => 1,
-            Entry::Num(n) => n + 1
-        };
-
-        for n in start as usize..(board.size()+1) {
-            board[row][col] = Entry::Num(n as u8);
-            if board.check_board() {
-                next_cord(&mut row, &mut col, board.size());
-                continue 'main;
-            }
-        }
-        board[row][col] = Entry::Empty;
-        prev_cord(&mut row, &mut col, board.size());
-    }
-}
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -51,7 +14,7 @@ fn main() {
         return
     }
     let size = args[1].parse().expect("The size should be an integer");
-    let mut b = board::Board::new(size);
+    let mut b = Board::new(size);
 
     let start = SystemTime::now();
     fill_board(&mut b);
---
+++ src/solver.rs
@@ -0,0 +1,65 @@
+extern crate test;
+
+use crate::board::{Board, Entry};
+
+fn next_cord(row: &mut usize, col: &mut usize, size: usize) {
+    if *col == size-1 {
+        *row += 1;
+        *col = 0;
+    } else {
+        *col += 1;
+    }
+}
+
+fn prev_cord(row: &mut usize, col: &mut usize, size: usize) {
+    if *col == 0 {
+        *row -= 1;
+        *col = size-1;
+    } else {
+        *col -= 1;
+    }
+}
+
+pub fn fill_board(board: &mut Board) {
+    let mut row = 0;
+    let mut col = 0;
+
+    'main: while row < board.size() && col < board.size() {
+        let start = match board[row][col] {
+            Entry::Empty => 1,
+            Entry::Num(n) => n + 1
+        };
+
+        for n in start as usize..(board.size()+1) {
+            board[row][col] = Entry::Num(n as u8);
+            if board.check_board() {
+                next_cord(&mut row, &mut col, board.size());
+                continue 'main;
+            }
+        }
+        board[row][col] = Entry::Empty;
+        prev_cord(&mut row, &mut col, board.size());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_fill_9(b: &mut Bencher) {
+        b.iter(|| {
+            let mut board = Board::new(9);
+            fill_board(&mut board);
+        });
+    }
+
+    #[bench]
+    fn bench_fill_16(b: &mut Bencher) {
+        b.iter(|| {
+            let mut board = Board::new(16);
+            fill_board(&mut board);
+        });
+    }
+}
