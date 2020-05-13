21fc00ee232946bf217a848c7dc6f2a275679447
Jakob Haahr Taankvist <jakob@jakob-lenovo.localdomain>
2020-05-13 13:00:55 +0200

    Remove feature test and format

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-#![feature(test)]
 mod board;
 mod solver;
-use board::{Board};
+use board::Board;
 use solver::fill_board;
-use std::time::{SystemTime};
 use std::fs::File;
 use std::io::Read;
+use std::time::SystemTime;
 
 use std::env;
 
@@ -20,12 +19,16 @@ fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() < 2 {
         println!("Usage: {} <soduku file>", args[0]);
-        return
+        return;
     }
     let mut b = load(&args[1]);
     println!("{}", b);
 
     let start = SystemTime::now();
     fill_board(&mut b);
-    println!("{} in {}", b, start.elapsed().expect("Time is wrong").as_millis());
+    println!(
+        "{} in {}",
+        b,
+        start.elapsed().expect("Time is wrong").as_millis()
+    );
 }
